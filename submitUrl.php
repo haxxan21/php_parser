4be8f8ee281889079504536c2f9c095587d2c769
haxxan21 <malikhassan053@gmail.com>
2020-01-06 17:14:32 +0500

    single file created

---
+++ submitUrl.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+        <form action="ExtractData.php" method="POST">
+        <div class="row">    
+            <div class="col-sm-6">
+                <div class="form-group">    
+                     <input type="text" class="form-control" name="url" placeholder="Enter URL to file">
+                 </div>
+            </div>
+            <div class="col-sm-6">
+                <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+        </div>
+        </form>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
